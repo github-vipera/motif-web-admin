af8b43295b7981bc119c0618c8b1d432d4733997
Marco Bonati <marco.bonati@vipera.com>
2018-11-27 15:03:50 +0100

    Added sort

--- src/app/sections/Sessions/components/sessions-section-component.ts
+++ src/app/sections/Sessions/components/sessions-section-component.ts
@@ -3,13 +3,11 @@ import { PluginView } from 'web-console-core'
 import { NGXLogger} from 'web-console-core'
 import { SecurityService } from '@wa-motif-open-api/security-service'
 import { SessionRow } from '../data/model'
-import { EditService, EditServiceConfiguration } from '../../../components/Grid/edit.service';
-import { GridComponent, GridDataResult } from '@progress/kendo-angular-grid';
+import { GridDataResult, DataStateChangeEvent } from '@progress/kendo-angular-grid';
 import { State, process } from '@progress/kendo-data-query';
-import { map } from 'rxjs/operators/map';
 import { WCToasterService } from 'web-console-ui-kit'
-import { Observable } from 'rxjs/Observable';
-import { forkJoin } from 'rxjs/observable/forkJoin'
+import { SortDescriptor, orderBy } from '@progress/kendo-data-query'
+
 
 const LOG_TAG = "[SessionsSection]";
 
@@ -23,13 +21,17 @@ const LOG_TAG = "[SessionsSection]";
   })
 export class SessionsSectionComponent implements OnInit {
 
+    private _data:any = [];
     public gridData: GridDataResult;
     public gridState: State = {
+        skip: 0,
+        take: 10
     };
+    public sort:SortDescriptor[] = [];
+    public sortOptions = { allowUnsort: 'true', mode: 'single' };
 
     constructor(private logger: NGXLogger, 
         private securityService:SecurityService,
-        public editService: EditService,
         private toaster: WCToasterService,
         private renderer:Renderer){
         this.logger.debug(LOG_TAG ,"Opening...");
@@ -41,20 +43,52 @@ export class SessionsSectionComponent implements OnInit {
     ngOnInit() {
         this.logger.debug(LOG_TAG ,"Initializing...");
         this.reloadSessionsList();
-
-        let data = [
-            
-        ];
-        this.gridData = process(data, this.gridState);
     }
 
     private reloadSessionsList():void {
         this.logger.debug(LOG_TAG ,"reloadSessionsList called...");
         this.securityService.getSessions().subscribe((data)=>{
             this.logger.debug(LOG_TAG ,"sessions: ", data);
-            this.gridData = process(data, this.gridState);
+            this.setData(data);
         }, (error)=>{
             this.logger.error(LOG_TAG ,"sessions error: ", error);
         });
     }
+    
+    /**
+     * Triggered by the grid component
+     * @param state 
+     */
+    public dataStateChange(state: DataStateChangeEvent): void {
+        this.gridState = state;
+        this.gridData = process(this._data, this.gridState);
+    }
+
+    /**
+     * Set the local data and refresh the grid
+     * @param data 
+     */
+    private setData(data:any):void {
+        this._data = data;
+        this.gridData = {
+            data: orderBy(this._data, this.sort),
+            total: this._data.length
+        };
+        //this.gridData = process(this._data, this.gridState);
+    }
+
+    private refreshData():void {
+        this.setData(this._data);
+    }
+
+    /**
+     * Triggered by the grid component
+     * @param sort 
+     */
+    public sortChange(sort: SortDescriptor[]): void {
+        this.logger.debug(LOG_TAG ,"sort change: ", sort);
+        this.sort = sort;
+        this.refreshData();
+    }
+
 }
