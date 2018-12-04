55f80206448e7eb4952dd32ceb5d5f2b493e679e
Marco Bonati <marco.bonati@vipera.com>
2018-12-04 10:03:39 +0100

    Added table model

---
+++ src/app/sections/ApplicationContent/data/model.ts
@@ -0,0 +1,8 @@
+export interface MobileApplicaton {
+    created : Date,
+    latestVersion : string,
+    forbiddenVersions : string,
+    downloadUrl : string,
+    lastAppCheck : Date,
+    name: string
+}
