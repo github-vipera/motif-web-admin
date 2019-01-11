87b04bf355b939ab2974966ef5fa8c3f0ad32878
Marco Bonati <marco.bonati@vipera.com>
2019-01-11 16:06:10 +0100

    Added filterable combobox feature

--- src/app/components/UI/selectors/domain-selector-combobox-component.ts
+++ src/app/components/UI/selectors/domain-selector-combobox-component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, OnDestroy, Input, Output, EventEmitter, forwardRef } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input, Output, EventEmitter, forwardRef, ViewChild } from '@angular/core';
 import { DomainsService, DomainsList, Domain } from '@wa-motif-open-api/platform-service';
 import { NGXLogger} from 'web-console-core';
 import { NotificationCenter, NotificationType } from '../../Commons/notification-center';
 import { NG_VALUE_ACCESSOR } from '@angular/forms';
 import { SubscriptionHandler } from '../../../components/Commons/subscription-handler';
+import { ComboBoxComponent } from '@progress/kendo-angular-dropdowns';
 
 const LOG_TAG = '[DomainSelectorComboBoxComponent]';
 
@@ -19,7 +20,7 @@ export const WC_DOMAIN_SELECTOR_CONTROL_VALUE_ACCESSOR: any = {
     styles: [
     ],
     template: `
-    <kendo-combobox style="width:100%;" [data]="domainList" 
+    <kendo-combobox #combo style="width:100%;" [data]="data"   [filterable]="true" (filterChange)="handleFilter($event)"
     [allowCustom]="false" [valueField]="'name'" 
     [textField]="'name'" [(ngModel)]="selectedDomain"></kendo-combobox>
     `,
@@ -27,11 +28,13 @@ export const WC_DOMAIN_SELECTOR_CONTROL_VALUE_ACCESSOR: any = {
 })
 export class DomainSelectorComboBoxComponent implements OnInit, OnDestroy {
 
+    public data: DomainsList = [];
     public domainList: DomainsList = [];
     public _selectedDomain: Domain; // combo box selection
     @Output() domainSelected: EventEmitter<Domain> = new EventEmitter();
     @Output() selectionCancelled: EventEmitter<any> = new EventEmitter();
     private _subHandler: SubscriptionHandler = new SubscriptionHandler();
+    @ViewChild('combo') combo: ComboBoxComponent;
 
     constructor(private logger: NGXLogger,
         private domainsService: DomainsService,
@@ -116,5 +119,11 @@ export class DomainSelectorComboBoxComponent implements OnInit, OnDestroy {
 
     registerOnTouched(fn: () => void): void { }
 
-
+    handleFilter(value) {
+        if (value.length >= 3) {
+            this.data = this.domainList.filter((s) => s.name.toLowerCase().indexOf(value.toLowerCase()) !== -1);
+        } else {
+            this.combo.toggle(false);
+        }
+    }
 }
