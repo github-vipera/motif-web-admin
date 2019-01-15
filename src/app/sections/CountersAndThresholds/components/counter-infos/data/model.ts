5be8019e6ce8976e071392f933cef7a2da90adf5
Marco Bonati <marco.bonati@vipera.com>
2019-01-15 12:26:28 +0100

    Minor UI improvements

--- src/app/sections/CountersAndThresholds/components/counter-infos/data/model.ts
+++ src/app/sections/CountersAndThresholds/components/counter-infos/data/model.ts
@@ -1,5 +1,6 @@
-import { CounterInfoEntityList } from '@wa-motif-open-api/counters-thresholds-service';
+import { CounterInfoEntityList, CounterInfoEntity } from '@wa-motif-open-api/counters-thresholds-service';
 import * as _ from 'lodash';
+import { String, StringBuilder } from 'typescript-string-operations';
 
 export class CounterInfosModel {
 
@@ -14,12 +15,32 @@ export class CounterInfosModel {
     }
 
     public loadData(data: CounterInfoEntityList) {
-        this._data = _.forEach(data, function(element) {
+        this._data = _.forEach(data, (element: CounterInfoEntity) => {
             if (element.created) {
                 element.created = new Date(element.created);
             }
+            element["pattern"] = this.buildPatternForItem(element);
         });
 
+    }
+
+    private buildPatternForItem(item: CounterInfoEntity): string {
+        let ret = String.Format('/{0}', item.channel);
+
+        if (item.domain) {
+            ret = String.Format('{0}/{1}', ret, item.domain);
+        }
+        if (item.domain && item.application) {
+            ret = String.Format('{0}/{1}', ret, item.application);
+        }
+        if (item.domain && item.application && item.service) {
+            ret = String.Format('{0}/{1}', ret, item.service);
+        }
+        if (item.domain && item.application && item.service && item.operation) {
+            ret = String.Format('{0}/{1}', ret, item.operation);
+        }
+
+        return ret;
     }
 
     public get data(): CounterInfoEntityList {
