bfd8dfc7fe2eafcc607cd426652ead27d39300db
Marco Bonati <marco.bonati@vipera.com>
2019-01-23 23:06:55 +0100

    Fixed drop file component

--- src/app/components/UI/file-drop-panel-component.ts
+++ src/app/components/UI/file-drop-panel-component.ts
@@ -2,6 +2,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { NGXLogger} from 'web-console-core'
 import { THROW_IF_NOT_FOUND } from '@angular/core/src/di/injector';
+import { DateFilterMenuInputComponent } from '@progress/kendo-angular-grid/dist/es2015/filtering/menu/date-filter-menu-input.component';
 
 
 const LOG_TAG = "[FileDropComponent]";
@@ -12,23 +13,28 @@ const LOG_TAG = "[FileDropComponent]";
         '.wc-file-drop-panel {  width: 100%; height: 100%; border: 1px dashed #8b8c8c; display:flex;align-items: center;justify-content: center; flex-direction:column;}',
         '.wc-file-drop-panel.dragover { background-color: #4c4c4c; }',
         '.wc-file-drop-panel-caption { font-size: .7rem;font-family: montserrat; text-transform: uppercase; vertical-align: middle; text-align: center;padding-top: 4px;}',
-        '.wc-file-drop-panel-caption-filename { font-size: .6rem;}'
+        '.wc-file-drop-panel-caption-filename { font-size: .6rem;}',
+        '.wc-file-drop-panel { font-family: montserrat; text-transform: uppercase; color: #888; font-size: .7rem; }'
     ],
     template:`
         <div droppable (filesDropped)="handleFilesDropped($event)" class="wc-file-drop-panel" role="button" #dropzone="droppable">
+        {{caption}}
+        <!--
         <label *ngIf="!dropzone.isHover" class="btn small wc-file-drop-panel-caption">{{caption}}</label>
         <label *ngIf="dropzone.isHover" class="btn small wc-file-drop-panel-caption">{{altCaption}}</label>
         <label *ngIf="droppedFileName" class="wc-file-drop-panel-caption wc-file-drop-panel-caption-filename">{{droppedFileName}}</label>
+        -->
         </div>
     `
 })
 export class FileDropPanelComponent implements OnInit {
 
-    @Input() caption:string = "Drop files here or click";
+    //@Input() caption:string = "Drop files here or click";
     @Input() altCaption:string = "Drop the file";
 
-    public droppedFileName:string;
+    public droppedFileName:string = null;
     private _droppedFile:File;
+    private _caption: string =  'Drop files here or click';
 
     constructor(private logger: NGXLogger){
             this.logger.debug(LOG_TAG ,"Creating...");
@@ -54,6 +60,19 @@ export class FileDropPanelComponent implements OnInit {
     public reset():void {
         this._droppedFile = null;
         this.droppedFileName = null;
+    }
+    
+    public get caption():string {
+        if (this.droppedFileName!=null){
+            return this.droppedFileName;
+        } else {
+            return this._caption;
+        }
+    }
+
+    @Input()
+    public set caption(value:string){
+        this._caption = value;
     }
 }
 
