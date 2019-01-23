4972271853c15c3a21553b1761100a6435436d8e
Marco Bonati <marco.bonati@vipera.com>
2019-01-23 15:41:16 +0100

    Added synthetic status

--- src/app/sections/WebContent/components/webcontent-section-component.ts
+++ src/app/sections/WebContent/components/webcontent-section-component.ts
@@ -1,12 +1,19 @@
 import { GridEditorCommandsConfig } from './../../../components/Grid/grid-editor-commands-group/grid-editor-commands-group-component';
-import { Component, OnInit, OnDestroy, ViewChild, Input, ElementRef, Renderer } from '@angular/core';
+import { Component, OnInit, OnDestroy, ElementRef } from '@angular/core';
 import { PluginView } from 'web-console-core';
 import { NGXLogger} from 'web-console-core';
 import { NotificationCenter, NotificationType } from '../../../components/Commons/notification-center';
-import { WebcontentService, BundleStatusList, BundleStatus } from '@wa-motif-open-api/web-content-service';
+import { WebcontentService, BundleStatusList, BundleStatus, ClusterBundleStatus } from '@wa-motif-open-api/web-content-service';
 import { SubscriptionHandler } from 'src/app/components/Commons/subscription-handler';
+import * as _ from 'lodash';
 
 const LOG_TAG = '[WebContentSectionComponent]';
+
+export enum PublishingStatus {
+    Published = 'PUBLISHED',
+    Unpublished = 'UNPUBLISHED',
+    Error = 'IN ERROR'
+}
 
 @Component({
     selector: 'wa-web-content-section',
@@ -14,7 +21,7 @@ const LOG_TAG = '[WebContentSectionComponent]';
     templateUrl: './webcontent-section-component.html'
   })
   @PluginView('WebContent', {
-    iconName: 'ico-web'
+    iconName: 'ico-web',
   })
 export class WebContentSectionComponent implements OnInit, OnDestroy {
 
@@ -71,6 +78,12 @@ export class WebContentSectionComponent implements OnInit, OnDestroy {
         this.loading = true;
         this._subHandler.add(this.webContentService.getBundlesStatusList().subscribe( (data: BundleStatusList) => {
             this.logger.debug(LOG_TAG, 'Get bundle statuses results:', data);
+
+            this.gridData = _.forEach(data, (element: BundleStatus) => {
+                element.info["syntheticStatus"] = this.buildSyntheticStatus(element);
+            });
+
+
             this.gridData = data;
             this.loading = false;
         }, (error) => {
@@ -87,5 +100,34 @@ export class WebContentSectionComponent implements OnInit, OnDestroy {
             });
         }));
     }
+
+    
+    private buildSyntheticStatus(statusInfo: BundleStatus): string {
+        let published: number = 0;  
+        let unpusblished: number = 0;  
+        for (let i=0;i<statusInfo.status.length;i++){
+            const clusterStatus:ClusterBundleStatus = statusInfo.status[i];
+            if (clusterStatus.status === PublishingStatus.Unpublished) {
+                unpusblished++;
+            }
+            if (clusterStatus.status === PublishingStatus.Published) {
+                published++;
+            }
+        }
+        this.logger.debug(LOG_TAG, 'buildSyntheticStatus (published count vs unpublished count): ', published, unpusblished);
+        if ((published===0) && (unpusblished > 0)) {
+            return PublishingStatus.Unpublished;
+        } else if ((unpusblished===0) && (published > 0)) {
+            return PublishingStatus.Published;
+        } else {
+            return PublishingStatus.Error;
+        }
+    }
+    
+
+    private buildSyntheticStatusXXX(statusInfo: BundleStatus): string {
+        return PublishingStatus.Error;
+    }
+
 
 }
